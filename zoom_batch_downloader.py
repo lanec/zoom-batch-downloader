cc0719e7c79b026e62d1d20e9ed0ad38bc6ae2f6
Anass <aness.zurba@gmail.com>
2023-09-23 13:56:24 +0300

    zoom_batch_downloader: v3
    better downloads with progress bar.
    use pagination as recommended by zoom API.

--- zoom_batch_downloader.py
+++ zoom_batch_downloader.py
@@ -4,12 +4,15 @@ import os
 import re
 import shutil
 import unicodedata
+import urllib
 from calendar import monthrange
+from json import dumps
 from time import sleep
 
 import colorama
 import requests
 from colorama import Fore, Style
+from tqdm import tqdm
 
 # Zoom API credentials
 ACCOUNT_ID = '##########'
@@ -46,30 +49,11 @@ MB = 1024 * KB
 GB = 1024 * MB
 TB = 1024 * GB
 
-# Chunk size in bytes, determines the rate at which progress is reported, you might want to change it depending
-# on your internet speed.
-CHUNK_SIZE = 1 * MB
-
 # Minimum free disk space in bytes for downloads to happen, downloading will be stalled if disk space is
 # expected to get below this amount as a result of the new file.
 MINIMUM_FREE_DISK = 1 * GB
 
 colorama.init()
-
-def get_headers(token):
-	return {
-		'Authorization': f'Bearer {token}',
-		'Content-Type': 'application/json'
-	} 
-
-def size_to_string(size_bytes):
-   if size_bytes == 0:
-       return '0', 'B'
-   units = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = 1024**i
-   size = round(size_bytes / p, 2)
-   return str(size), units[i]
 
 def main():
 	from_date = datetime.datetime(START_YEAR , START_MONTH, START_DAY or 1)
@@ -92,33 +76,33 @@ def main():
 		user_description = get_user_description(user_email, user_name)
 		user_host_folder = get_user_host_folder(user_email)
 
-		print(Style.BRIGHT)
-		print(f'Downloading videos from user {user_description} - Starting at {from_date_string} and up to (inclusive) {to_date_string}.')
-		print(Style.RESET_ALL)
+		user_message = f'Downloading videos from user {user_description} - Starting at {from_date_string} and up to (inclusive) {to_date_string}.'
+		print(Style.BRIGHT + user_message + Style.RESET_ALL)
+		print()
+	
+		meetings = get_meetings(user_email, from_date_string, to_date_string)
+		user_file_count, user_total_size, user_skipped_count = download_recordings(meetings, user_host_folder)
 
-		url = f'https://api.zoom.us/v2/users/{user_email}/recordings?from={from_date_string}&to={to_date_string}&page_size=300'
-
-		if VERBOSE_OUTPUT:
-			print(f'{Style.DIM}Searching: {url}{Style.RESET_ALL}')
-
-		data = get_with_token(lambda t: requests.get(url, headers=get_headers(t))).json()
-
-		user_file_count, user_total_size, user_skipped_count = get_recordings(data, user_host_folder)
-
+		print()
 		print(f'{Style.BRIGHT}==============================================================={Style.RESET_ALL}')
+		print()
 		
+		file_count += user_file_count
+		total_size += user_total_size
+		skipped_count += user_skipped_count
 
-	file_count += user_file_count
-	total_size += user_total_size
-	skipped_count += user_skipped_count
+	total_size_str = size_to_string(total_size)
 
-	total_size_str = ''.join(size_to_string(total_size))
-
-	print()
 	print(
 		f'{Style.BRIGHT}Downloaded {Fore.GREEN}{file_count}{Fore.RESET} files.',
 		f'Total size: {Fore.GREEN}{total_size_str}{Fore.RESET}.{Style.RESET_ALL}',
 		f'Skipped: {skipped_count} files.'
+	)
+
+def get_users():
+	return paginate_reduce(
+		'https://api.zoom.us/v2/users', [],
+		lambda users, page: users + [(user['email'], get_user_name(user)) for user in page['users']]
 	)
 
 def get_user_description(user_email, user_name):
@@ -129,7 +113,27 @@ def get_user_host_folder(user_email):
 		return os.path.join(PATH, user_email)
 	else:
 		return PATH
+
+def get_meetings(user_email, from_date_string, to_date_string):
+	url = f'https://api.zoom.us/v2/users/{user_email}/recordings?from={from_date_string}&to={to_date_string}'
 	
+	return paginate_reduce(url, [], lambda meetings, page: meetings + page['meetings'])
+
+def get_headers(token):
+	return {
+		'Authorization': f'Bearer {token}',
+		'Content-Type': 'application/json'
+	} 
+
+def size_to_string(size_bytes):
+   if size_bytes == 0:
+       return '0B'
+   units = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = 1024**i
+   size = round(size_bytes / p, 2)
+   return f'{size}{units[i]}'
+
 def get_with_token(get):
 	cached_token = getattr(get_with_token, 'token', '')
 	response = get(cached_token)
@@ -153,23 +157,67 @@ def fetch_token():
 		raise Exception(f'Unable to fetch access token: {response["reason"]} - verify your credentials.')
 
 	return response['access_token']
-	
-def get_users():
-	users_data = get_with_token(
-		lambda t: requests.get(url='https://api.zoom.us/v2/users', headers=get_headers(t))
+
+def paginate_reduce(url, initial, reduce):
+	initial_url = add_url_params(url, {'page_size': 1})
+	page = get_with_token(
+		lambda t: requests.get(url=initial_url, headers=get_headers(t))
 	).json()
 
-	all_users = []
+	result = initial
 
-	page_count = int(users_data['page_count'])
-	for page_number in range(1, page_count + 1):
-		page = get_with_token(
-			lambda t: requests.get(url=f'https://api.zoom.us/v2/users?page_number={page_number}', headers=get_headers(t))
-		).json()
+	while page:
+		result = reduce(result, page)
 
-		all_users += [(user['email'], get_user_name(user)) for user in page['users']]
+		next_page_token = page['next_page_token']
+		if next_page_token:
+			next_url = add_url_params(url, {'page_token': next_page_token})
+			page = get_with_token(lambda t: requests.get(next_url, headers=get_headers(t))).json()
+		else:
+			page = None
 
-	return all_users
+	return result
+
+def add_url_params(url, params):
+    """ Add GET params to provided URL being aware of existing.
+
+    :param url: string of target URL
+    :param params: dict containing requested params to be added
+    :return: string with updated URL
+    
+    >> url = 'https://stackoverflow.com/test?answers=true'
+    >> new_params = {'answers': False, 'data': ['some','values']}
+    >> add_url_params(url, new_params)
+    'https://stackoverflow.com/test?data=some&data=values&answers=false'
+    """
+    # Unquoting URL first so we don't lose existing args
+    url = urllib.parse.unquote(url)
+    # Extracting url info
+    parsed_url = urllib.parse.urlparse(url)
+    # Extracting URL arguments from parsed URL
+    get_args = parsed_url.query
+    # Converting URL arguments to dict
+    parsed_get_args = dict(urllib.parse.parse_qsl(get_args))
+    # Merging URL arguments dict with new params
+    parsed_get_args.update(params)
+
+    # Bool and Dict values should be converted to json-friendly values
+    # you may throw this part away if you don't like it :)
+    parsed_get_args.update(
+        {k: dumps(v) for k, v in parsed_get_args.items()
+         if isinstance(v, (bool, dict))}
+    )
+
+    # Converting URL argument to proper query string
+    encoded_get_args = urllib.parse.urlencode(parsed_get_args, doseq=True)
+    # Creating new parsed result object based on provided with new
+    # URL arguments. Same thing happens inside urlparse.
+    new_url = urllib.parse.ParseResult(
+        parsed_url.scheme, parsed_url.netloc, parsed_url.path,
+        parsed_url.params, encoded_get_args, parsed_url.fragment
+    ).geturl()
+
+    return new_url
 
 def get_user_name(user_data):
 	first_name = user_data.get("first_name")
@@ -180,13 +228,10 @@ def get_user_name(user_data):
 	else:
 		return first_name or last_name
 
-def get_recordings(data, host_folder):
+def download_recordings(meetings, host_folder):
 	total_size, file_count, skipped_count = 0, 0, 0
 
-	if 'code' in data:
-		raise Exception(f'{data["code"]} {data["message"]}')
-
-	for meeting in data['meetings']:
+	for meeting in meetings:
 		for record in meeting['recording_files']:
 			if record['status'] != 'completed':
 				continue
@@ -242,72 +287,91 @@ def create_path(host_folder, file_name, topic_name, record_name):
 
 def download_recording(download_url, host_folder, file_name, file_size, topic_name, meeting_name):
 	if VERBOSE_OUTPUT:
-		print(Style.BRIGHT + f'Found: {download_url}' + Style.RESET_ALL)
+		print()
+		print(Style.DIM + f'Found: {download_url}' + Style.RESET_ALL)
 
 	file_path = create_path(host_folder, file_name, topic_name, meeting_name)
 
 	if os.path.exists(file_path) and os.path.getsize(file_path) == file_size:
 		print(f'{Style.DIM}Skipping existing file: {file_name}{Style.RESET_ALL}')
 		return False
+	elif os.path.exists(file_path):
+		print(f'{Style.DIM}{Fore.RED}Deleting corrupt file: {file_name}{Style.RESET_ALL}')
+		os.remove(file_path)
 
 	print(f'{Style.BRIGHT}Downloading: {file_name}{Style.RESET_ALL}')
 
 	wait_for_disk_space(file_size)
 
-	response = get_with_token(
-		lambda t: requests.get(f'{download_url}?access_token={t}', stream=True)
-	)
-
 	tmp_file_path = file_path + '.tmp'
-	save_to_disk(response, tmp_file_path, file_size)
+	do_with_token(lambda t: download(f'{download_url}?access_token={t}', tmp_file_path))
+	
 	os.rename(tmp_file_path, file_path)
 
 	return True
 
 def wait_for_disk_space(file_size):
-	file_size_str = ''.join(size_to_string(file_size))
+	file_size_str = size_to_string(file_size)
 
 	free_disk = shutil.disk_usage(PATH)[2]
-	free_disk_str = ''.join(size_to_string(free_disk))
+	free_disk_str = size_to_string(free_disk)
+	required_disk_space = file_size + MINIMUM_FREE_DISK
 
 	while free_disk < file_size + MINIMUM_FREE_DISK:
-		print(f'{Fore.RED + Style.BRIGHT}Waiting for disk space... (File size: {file_size_str}, available: {free_disk_str}){Style.RESET_ALL}')
-		sleep(30)
+		print(
+			f'{Fore.RED + Style.BRIGHT}Waiting for disk space... '
+			f'(File size: {file_size_str}, minimum free disk space: {size_to_string(MINIMUM_FREE_DISK)}, '
+			f'available: {free_disk_str}/{size_to_string(required_disk_space)}){Style.RESET_ALL}'
+		)
+		sleep(15)
 		free_disk = shutil.disk_usage(PATH)[2]
-		free_disk_str = ''.join(size_to_string(free_disk))
+		free_disk_str = size_to_string(free_disk)
 
-def save_to_disk(response, file_path, file_size):
-	file_size_str = ''.join(size_to_string(file_size))
+def do_with_token(do):
+	def do_wrapper(token):
+		test_response = requests.get('https://api.zoom.us/v2/users/me/recordings', headers=get_headers(token))
+		if test_response.ok:
+			do(token)
 
-	size, size_length = 0, 0
-
-	print(Fore.CYAN, end='')
-
-	with open(file_path, 'wb') as file:
-		size_str1, size_str2 = size_to_string(size)
-		print(f'\r{size_str1: <{size_length}}{size_str2} / {file_size_str}', end='\r')
+		return test_response
 		
-		chunk: int
-		for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-			size += len(chunk)
-			if size >= file_size:
-				print(Fore.GREEN, end='')
+	get_with_token(lambda t: do_wrapper(t))
 
-			size_str1, size_str2 = size_to_string(size)
-			size_length = max(size_length, len(size_str1))
-			print(f'\r{size_str1: <{size_length}}{size_str2} / {file_size_str}', end='\r')
+def download(url, output_path):
+	class DownloadProgressBar(tqdm):
+		def update_to(self, b=1, bsize=1, tsize=None):
+			if tsize is not None:
+				self.total = tsize
+			self.update(b * bsize - self.n)
 
-			file.write(chunk)
-
-	print(Style.RESET_ALL)
+	with DownloadProgressBar(
+		unit='B', unit_divisor=1024, unit_scale=True, miniters=1
+	) as t:
+		try:
+			urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+			size = os.path.getsize(output_path)
+			t.update_to(bsize=size, tsize=size)
+		except:
+			try:
+				os.remove(output_path)
+			except OSError:
+				pass
+			
+			raise
 	   
 if __name__ == '__main__':
 	try:
-		main()
-	except Exception as error:
+		try:
+			main()
+		except Exception as error:
+			print()
+			print(f'{Fore.RED + Style.BRIGHT}Error: {error}{Style.RESET_ALL}')
+			print()
+		
 		print()
-		print(f'{Fore.RED + Style.BRIGHT}Error: {error}{Style.RESET_ALL}')
-		print()
+		input('Press Enter to exit...')
 
-	print()
-	input('Press Enter to exit...')
+	except KeyboardInterrupt:
+		print()
+		print(f'{Fore.RED + Style.BRIGHT}Interrupted by the user{Style.RESET_ALL}')
+		exit(1)
