35c3e49b7b6240f316b0de22f90aede545dc8f51
Anass Zurba <aness.zurba@gmail.com>
2023-09-28 22:37:20 +0300

    config_template: because reasons.

--- config_template.py
+++ config_template.py
@@ -1,10 +1,10 @@
 # Zoom API credentials.
-ACCOUNT_ID = r"##########"
-CLIENT_ID = r"##########"
-CLIENT_SECRET = r"##########"
+ACCOUNT_ID = R"##########"
+CLIENT_ID = R"##########"
+CLIENT_SECRET = R"##########"
 
 # Put your own download path here, no need to escape backslashes but avoid ending with one.
-OUTPUT_PATH = r"C:\Test\Zoom"
+OUTPUT_PATH = R"C:\Test\Zoom"
 
 # Date range (inclusive) for downloads, None value for Days gets replaced by first/last day of the month.
 START_DAY, START_MONTH, START_YEAR = None, 5, 2020
@@ -12,24 +12,24 @@ END_DAY, END_MONTH, END_YEAR = None , 3, 2022
 
 # Put here emails of the users you want to check for recordings. If empty, all users under the account will be checked.
 USERS = [
-	# r"####@####.####",
-    # r"####@####.####",
+	# R"####@####.####",
+    # R"####@####.####",
 ]
 
 # Put here the topics of the meetings you wish to download recordings for. If empty, no topic filtering will happen.
 TOPICS = [
-    # r"############",
-    # r"############",
+    # R"############",
+    # R"############",
 ]
 
 # Put here the file types you wish to download. If empty, no file type filtering will happen.
 RECORDING_FILE_TYPES = [
-    # r"MP4",            # Video file of the recording.
-    # r"M4A",            # Audio-only file of the recording.
-    # r"TRANSCRIPT",     # Transcription file of the recording in VTT format.
-    # r"CHAT",           # A TXT file containing in-meeting chat messages that were sent during the meeting.
-    # r"CSV",            # File containing polling data in CSV format.
-    # r"SUMMARY",        # Summary file of the recording in JSON file format.
+    # R"MP4",            # Video file of the recording.
+    # R"M4A",            # Audio-only file of the recording.
+    # R"TRANSCRIPT",     # Transcription file of the recording in VTT format.
+    # R"CHAT",           # A TXT file containing in-meeting chat messages that were sent during the meeting.
+    # R"CSV",            # File containing polling data in CSV format.
+    # R"SUMMARY",        # Summary file of the recording in JSON file format.
 ]
 
 # If true, recordings will be grouped in folders by their owning user.
